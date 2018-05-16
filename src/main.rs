bf927cbc13854bd0774fc3284342a27c5f5a27fe
Chris Williams <chrispwill@gmail.com>
2018-05-16 10:26:10 +1000

    Make API versioned

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #[macro_use]
 extern crate rouille;
 
-pub mod routes;
+pub mod v1;
 
-use routes::auth_routes;
+use rouille::{Response, Request};
 
 fn main() {
     rouille::start_server("localhost:8000", move |request| {
-        auth_routes(&request)
+        routes(&request)
     });
+}
+
+fn routes(request: &Request) -> Response {
+    router!(
+        request,
+        (GET) (/) => {
+            Response::empty_404()
+        },
+        _ => {
+            if let Some(v1_request) = request.remove_prefix("/v1") {
+                v1::v1_routes(&v1_request)
+            } else {
+                Response::empty_404()
+            }
+        },
+    )
 }
