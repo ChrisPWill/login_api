c17527ab07320d7b7fe10c7c67573445e8eb0fee
Chris Williams <chrispwill@gmail.com>
2018-05-17 13:20:30 +1000

    Add auth_tokens table

---
+++ migrations/2018-05-17-025458_auth_tokens/up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE auth_tokens (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL
+        CONSTRAINT fk_auth_tokens_user_id REFERENCES users(id),
+    token UUID NOT NULL,
+    date_created TIMESTAMP WITH TIME ZONE NOT NULL
+        CONSTRAINT df_auth_tokens_date_created DEFAULT (now() AT TIME ZONE 'utc'),
+    date_expired TIMESTAMP WITH TIME ZONE NOT NULL,
+    token_type VARCHAR(15) NOT NULL
+);
+
+CREATE INDEX ix_auth_tokens_token ON auth_tokens USING hash (token);
